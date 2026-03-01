ef736510098d76bf7cc63a81156a5f46d48c8e3e
mesconliska-maker <mescon.liska@gmail.com>
2026-03-01 17:33:49 +0100

    Update ContactPage.tsx

--- src/app/pages/ContactPage.tsx
+++ src/app/pages/ContactPage.tsx
@@ -243,7 +243,7 @@ export function ContactPage() {
         {/* Map */}
         <motion.div {...fadeInUp} className="h-[500px] rounded-lg overflow-hidden shadow-lg">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.9!2d12.7733!3d49.7511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDnCsDQ1JzA0LjAiTiAxMsKwNDYnMjMuOSJF!5e0!3m2!1sen!2scz!4v1234567890"
+            src="https://www.google.com/maps?q=n%C3%A1m%C4%9Bst%C3%AD+Republiky+286,+348+02+Bor+u+Tachova,+Czech+Republic&output=embed"
             width="100%"
             height="100%"
             style={{ border: 0 }}
